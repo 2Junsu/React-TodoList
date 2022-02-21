8b1dbff8050a1221346c93041fd67ce25d6cfaca
2Junsu <ijh1205@naver.com>
2022-02-22 01:44:57 +0900

    Remove: completedTodo 컴포넌트 및 관련 Redux 코드 삭제

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
-import Todo from './Todo'
-import TagModal from './TagModal'
-import CompletedTodo from './CompletedTodo'
-import Modal from './Modal'
-export { Todo, TagModal, CompletedTodo, Modal }
+import Todo from "./Todo"
+import TagModal from "./TagModal"
+import Modal from "./Modal"
+export { Todo, TagModal, Modal }
